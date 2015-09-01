17875889515be8b6885bea0bf24dd9f2f4d199ec
Skobcow Roman <djromeo.91@mail.ru>
2015-09-01 14:15:00 +0300

    Signed-off-by: Skobcow Roman <djromeo.91@mail.ru>

--- portfol/ttt/bud_files/widgets.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.cards').on('click', 'a[data-action="expand"]', function (e) {
-        $(this).parent().toggleClass('is-active')
-                .parent().parent().toggleClass('is-expanded');
-        e.preventDefault();
-    });
-});
