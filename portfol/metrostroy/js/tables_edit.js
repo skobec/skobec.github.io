ccc1a8779b4b82298963afd68f647bed70884c12
Skobcow Roman <djromeo.91@mail.ru>
2015-12-04 16:38:28 +0300

    report

--- portfol/metrostroy/js/tables_edit.js
+++ portfol/metrostroy/js/tables_edit.js
@@ -8,6 +8,11 @@ function addRowToTable2(table, cell1, cell2, cell3, cell4) {
     row = "<tr><td><span style='text-align: left'>" + cell1 + "</span></td><td><span style='text-align: center'>" + cell2 +" "+ cell3 +"</span></td><td><span>" + ('<a href="#" class="btn_edit"></a><a href="#" class="btn_del_zap"></a>') + "</span></td></tr>";
     table.append(row);
 }
+function addRowToTable3(table, cell1,cell2) {
+    var row;
+    row = "<tr><td><span style='text-align: left'>" + cell1 + "</span></td><td><span style='text-align: center'>" + cell2 +"</span></td><td><span>" + ('<a href="#" class="btn_edit"></a><a href="#" class="btn_del_zap"></a>') + "</span></td></tr>";
+    table.append(row);
+}
 $(document).ready(function() {
 
     //$("#table_add_plans .btn_del_zap").on("click",function() {
