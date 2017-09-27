7913ff98b5f11a94e363238e2aeb467ae44fde85
skobtsov.r <djromeo.91@mail.ru>
2017-09-27 07:36:26 +0300

    del and push

--- portfol/beznadega727.github.io/reader/FB3ReaderHeaders.js
+++
@@ -1,7 +0,0 @@
-/// <reference path="Reader/FB3ReaderHead.ts" />
-/// <reference path="Site/FB3ReaderSiteHead.ts" />
-/// <reference path="DOM/FB3DOMHead.ts" />
-/// <reference path="DataProvider/FB3DataProviderHead.ts" />
-/// <reference path="Bookmarks/FB3BookmarksHead.ts" />
-/// <reference path="PagesPositionsCache/PPCacheHead.ts" />
-//# sourceMappingURL=FB3ReaderHeaders.js.map
